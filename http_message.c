2e24749c8269112a6c14181e50976a3ffc8d1bbe
d_rue <danieldipiet@gmail.com>
2024-11-11 00:31:30 -0800

    Version 1

---
+++ http_message.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "http_message.h"
+
+void read_http_client_message(int sock_fd, http_client_message_t** msg, http_read_result_t* result)
+{
+    char buffer[1024];
+    int bytes_read = read(sock_fd, buffer, sizeof(buffer));
+    if (bytes_read == -1)
+    {
+        perror("read\n");
+        return 1;
+    }
+
+    buffer[bytes_read] = '\0';
+    printf("recieved %d bytes: %s\n", bytes_read, buffer);
+    *msg = malloc(sizeof(http_client_message_t));
+    if(*msg == NULL)
+    {
+        *result = BAD_REQUEST;
+        return;
+    }
+
+    *result = CLOSE_CONNECTION;
+}
+void http_client_message_free(http_client_message_t* msg)
+{
+    free(msg);
+}
